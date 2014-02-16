953400f5cb2b2abeb6b164bfb7ef162002bfb827
Yasunori Suzuki <railsyas@gmail.com>
2014-02-16 21:34:55 +0900

    Changed some messages.

--- upload.rb
+++ upload.rb
@@ -2,5 +2,6 @@
 require '/mnt/sd/sources/twitter.rb'
 require '/mnt/sd/sources/post_to_box.rb'
 
-Twitter.new.post('Upload program is launched!')
+Twitter.new.post('Hello!')
 PostToBox.new.execute
+Twitter.new.post('Goodbye!')
